f9c3295c47a0d1c782e7a87b39dfe27223fd0738
Lor-Web <lordesweb@gmail.com>
2026-04-16 16:16:30 +0300

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import type React from "react";
 
+import * as styles from "./app.module.scss";
+
 const App: React.FC = () => {
-	return <>hi app</>;
+	return <div className={styles.test}>hi app</div>;
 };
 
 export default App;
